0b7730999f2da0d1a8259161467d5f50e5103660
Lucas Nascimento Camolezi <camolezi@usp.br>
2018-05-26 17:49:49 -0300

    Sistema Basico de pegar itens e lançamento implementado

---
+++ FirstPersonBaby/Assets/Scripts/PickObjects.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickObjects : MonoBehaviour {
+
+    // Use this for initialization
+
+    bool segurandoObj = false;
+    bool podePegar = false;
+    GameObject objetoAPegar;
+    Transform MainCamera;
+    public float forca = 1.0f;
+
+    void Start() {
+
+        MainCamera = Camera.main.transform;
+ 
+
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if(Input.GetButtonDown("Fire1")) {
+
+            if (!segurandoObj) {
+
+                if (podePegar) {
+
+                    pegarObjeto();
+
+                }
+
+            }else {
+
+                jogarObjeto();
+
+
+            }
+        }
+       
+    }
+
+    void pegarObjeto() {
+
+        segurandoObj = true;
+
+        objetoAPegar.SetActive(false);
+
+
+      
+        
+
+    }
+
+
+    void jogarObjeto() {
+
+        segurandoObj = false;
+
+        objetoAPegar.SetActive(true);
+
+        objetoAPegar.transform.position = MainCamera.transform.position + new Vector3(0,0,0.5f);
+
+        objetoAPegar.GetComponent<Rigidbody>().AddForce(MainCamera.forward * forca, ForceMode.Impulse);
+
+    }
+    
+    
+    void OnTriggerEnter(Collider objeto) {
+
+        podePegar = true;
+
+        objetoAPegar = objeto.gameObject;
+
+
+    }
+
+    void OnTriggerExit(Collider objeto) {
+
+        podePegar = false;
+
+            
+    }
+
+
+
+
+
+
+}
