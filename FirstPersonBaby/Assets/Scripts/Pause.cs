5772a2b845b1af33005877a7d8d906815bc44b22
Unknown <gymori@usp.br>
2018-09-09 19:30:50 -0300

    botao de sair implementado

--- FirstPersonBaby/Assets/Scripts/Pause.cs
+++ FirstPersonBaby/Assets/Scripts/Pause.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Pause : MonoBehaviour {
 
@@ -10,9 +11,12 @@ public class Pause : MonoBehaviour {
     GameObject pausemenu;
 
 	void Start () {
+
+        Button quit_button = GameObject.Find("Quit Button").GetComponent<Button>();
         camerascript = GameObject.FindWithTag("MainCamera").GetComponent<CameraScript>();
         pickobjects = GameObject.FindWithTag("AreaPickUp").GetComponent<PickObjects>();
         pausemenu = GameObject.Find("Pause Menu");
+        quit_button.onClick.AddListener(QuitGame);
         pausemenu.SetActive(false);
 	}
 	
@@ -23,9 +27,13 @@ public class Pause : MonoBehaviour {
             {
                 Time.timeScale = 0;
                 paused = true;
-                camerascript.enabled = false;
+           
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+                 camerascript.enabled = false;
                 pickobjects.enabled = false;
                 pausemenu.SetActive(true);
+                
             }
         }else
         {
@@ -34,9 +42,18 @@ public class Pause : MonoBehaviour {
                 Time.timeScale = 1;
                 paused = false;
                 camerascript.enabled = true;
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+                camerascript.cursorIsLocked = true;
                 pickobjects.enabled = true;
                 pausemenu.SetActive(false);
+                
             }
         }
 	}
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
 }
