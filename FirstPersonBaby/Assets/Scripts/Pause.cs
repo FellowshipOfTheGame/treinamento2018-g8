72f8021631c2f9aa27adccf39ada062b214c23a7
Unknown <10716629@usp.br>
2018-09-10 23:24:17 -0300

    Pause funcional com clone

--- FirstPersonBaby/Assets/Scripts/Pause.cs
+++ FirstPersonBaby/Assets/Scripts/Pause.cs
@@ -7,13 +7,22 @@ public class Pause : MonoBehaviour {
 
     bool paused = false;
     CameraScript camerascript;
+    CameraScript camerascript_clone;
     PickObjects pickobjects;
     GameObject pausemenu;
 
 	void Start () {
 
         Button quit_button = GameObject.Find("Quit Button").GetComponent<Button>();
-        camerascript = GameObject.FindWithTag("MainCamera").GetComponent<CameraScript>();
+        camerascript = GameObject.Find("Player").transform.Find("Main Camera").GetComponent<CameraScript>();
+        if(GameObject.Find("BrinquedoRobo") != null)
+        {
+            camerascript_clone = GameObject.Find("BrinquedoRobo").transform.Find("CameraRobo").GetComponent<CameraScript>();
+        }
+        else
+        {
+            camerascript_clone = null;
+        }
         pickobjects = GameObject.FindWithTag("AreaPickUp").GetComponent<PickObjects>();
         pausemenu = GameObject.Find("Pause Menu");
         quit_button.onClick.AddListener(QuitGame);
@@ -30,7 +39,11 @@ public class Pause : MonoBehaviour {
            
                 Cursor.lockState = CursorLockMode.None;
                 Cursor.visible = true;
-                 camerascript.enabled = false;
+                camerascript.enabled = false;
+                    if(camerascript_clone != null)
+                {
+                    camerascript_clone.enabled = false;
+                }
                 pickobjects.enabled = false;
                 pausemenu.SetActive(true);
                 
@@ -42,6 +55,10 @@ public class Pause : MonoBehaviour {
                 Time.timeScale = 1;
                 paused = false;
                 camerascript.enabled = true;
+                if (camerascript_clone != null)
+                {
+                    camerascript_clone.enabled = true;
+                }
                 Cursor.lockState = CursorLockMode.Locked;
                 Cursor.visible = false;
                 camerascript.cursorIsLocked = true;
